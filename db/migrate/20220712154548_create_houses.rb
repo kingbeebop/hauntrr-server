37c19d20921054254a93a9fdb37b8932ed531e5e
Brendan Ward <kingbeebop@gmail.com>
2022-07-12 16:39:29 -0400

    ok

--- db/migrate/20220712154548_create_houses.rb
+++ db/migrate/20220712154548_create_houses.rb
@@ -3,6 +3,8 @@ class CreateHouses < ActiveRecord::Migration[6.1]
     create_table :houses do |t|
       t.string :name
       t.string :location
+      t.integer :distance
+      t.string :description
       t.string :picUrl
       t.integer :user_id
     end
